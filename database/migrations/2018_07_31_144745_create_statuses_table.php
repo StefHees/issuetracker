a92a754d5e624445f88a49977f96347ff26551f8
PC_VAN_THOMAS\Thomas <doubtingguy@hotmail.nl>
2018-07-31 17:23:10 +0200

    no message

---
+++ database/migrations/2018_07_31_144745_create_statuses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('status_name');
+            $table->string('status_description');
+            $table->string('dashboard_color_hex');
+            $table->double('dashboard_order', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
