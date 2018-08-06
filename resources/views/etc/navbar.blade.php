5bd2e73e21d580be3169fcc42a1c91bf9bb8e79f
Stef Hees <stefhees@gmail.com>
2018-08-06 10:27:38 +0200

    css

--- resources/views/etc/navbar.blade.php
+++ resources/views/etc/navbar.blade.php
@@ -1,11 +1,14 @@
-<div class="project p-2">
-    <div class="">
-        <img class="rounded-circle logo" src="images/filler-logo.jpg">
-    </div>
-    <h5>Memes in Space</h5>
-    <p class="font-weight-bold text-muted"></p>
+<div class="user-nav">
+    @guest
+        <div class="p-3 row justify-content-center">
+            <a class="btn btn-primary text-white m-1" href="{{ route('login') }}">{{ __('Login') }}</a>
+            <a class="btn btn-primary text-white m-1" href="{{ route('register') }}">{{ __('Register') }}</a>
+        </div>
+    @else
+        <h3>{{ Auth::user()->name }}</h3>
+        <a class="btn btn-primary text-white" href="{{ route('logout') }}">logout</a>
+    @endguest
 </div>
-
 <ul class="nav flex-column">
     <li class="nav-item">
         <a class="nav-link" href="{{ route('issues.index') }}"> Issues</a>
@@ -14,7 +17,7 @@
         <a class="nav-link" href="{{ route('clients.index') }}">Clients</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="{{ route('statuses.index') }}">Statuses</a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="">Disabled</a>
