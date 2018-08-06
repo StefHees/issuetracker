e260c148096a0b657d050e9318ec252910916ad7
Joost Koopmans <joost@inovamedia.nl>
2018-08-06 09:59:18 +0200

    revert back merge conflict tests

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 
 
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('test-conflict');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resources([
     'clients' => 'ClientController',
