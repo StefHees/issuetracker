8a5941b4214ff51c8fad30e22535f42b81665610
PC_VAN_THOMAS\Thomas <doubtingguy@hotmail.nl>
2018-08-15 14:57:24 +0200

    Add parent_id field and creat "internal" FK relation (relation between records)

---
+++ database/migrations/2018_08_15_123517_alter_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->unsignedInteger('parent_id')->nullable();
+
+            $table->foreign('parent_id')
+                ->references('id')
+                ->on('comments')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropForeign('comments_parent_id_foreign');
+            $table->dropColumn('parent_id');
+        });
+    }
+}
